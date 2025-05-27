8073c188c3d630489530eb523b1952673a72016e
Vladislav Nachikov <vlnachikov@gmail.com>
2025-05-27 16:30:03 +0200

    vlad-nachikov-w3-JavaScript

--- 1-JavaScript/Week3/assignment/ex5-wallet/index.js
+++ 1-JavaScript/Week3/assignment/ex5-wallet/index.js
@@ -75,7 +75,7 @@ const quiz = {
       b: 'cash, name', 
       c: 'amount, this, wallet'
     },
-    answer: '?',
+    answer: 'b',
   },
   q2: {
     question: 'What is in the Call Stack, from top to bottom?',
@@ -84,7 +84,7 @@ const quiz = {
       b: 'anonymous, transferInto', 
       c: 'transferInto, anonymous' 
     },
-    answer: '?',
+    answer: 'a',
   },
   q3: {
     question: 'What tooltip appears when hovering over the third debug button?',
@@ -93,7 +93,7 @@ const quiz = {
       b: 'Step out of current function', 
       c: 'Step' 
     },
-    answer: '?',
+    answer: 'a',
   },
   q4: {
     question: 'What is displayed in the console?',
@@ -102,7 +102,7 @@ const quiz = {
       b: 'Transferring € 50,00 from Jack to undefined', 
       c: 'Transferring € 50,00 from Jack to Jane' 
     },
-    answer: '?',
+    answer: 'b',
   },
   q5: {
     question: 'The owner of the wallet with insufficient funds is:',
@@ -111,6 +111,6 @@ const quiz = {
       b: 'Joe', 
       c: 'Jane' 
     },
-    answer: '?',
+    answer: 'c',
   },
 };
